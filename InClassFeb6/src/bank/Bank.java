32840bdf148de1dee1c36d18f3993ac39d78947f
shieldbe <shieldbe@MCD210-PC-03.uccc.uc.edu>
2020-02-06 11:50:11 -0500

    Declared and Instantiated objects in Main.

--- InClassFeb6/src/bank/Bank.java
+++ InClassFeb6/src/bank/Bank.java
@@ -1,5 +1,24 @@
 package bank;
-
+//model a bank corporation like 5/3rd
 public class Bank {
-
+private String name;
+/***
+ * get name of bank
+ * @return
+ */
+public String getName() {
+	return name;
 }
+/***
+ * set name of bank
+ * @param name
+ */
+public void setName(String name) {
+	this.name = name;
+}
+
+public String toString() {
+	return "Name = " + name + super.toString();
+}
+}
+
--- InClassFeb6/src/main/Main.java
+++ InClassFeb6/src/main/Main.java
@@ -1,9 +1,19 @@
 package main;
 
+import bank.Bank;
+import bank.BrickAndMortar;
+import bank.CafeBranch;
+import bank.Online;
+
 public class Main {
 
 	public static void main(String[] args) {
+//declare and instantiate a bank, online, brickandmortar, and cafebranch
+Bank PNC = new Bank();
+Online WebURL = new Online();
+BrickAndMortar Building = new BrickAndMortar();
+CafeBranch Starbucks = new CafeBranch();
 
-}
+	}
 
 }
